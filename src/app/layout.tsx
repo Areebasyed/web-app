6b42787aa2b55b203bfcea01b5507e9d553059bc
iamzainn <iamzainnf@gmail.com>
2024-10-15 21:03:48 +0500

    rm system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <body className={cn("font-sans antialiased", inter.variable)}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
